ae2a02ea4dc6802a0d3d13d9fe066a6ed24adbe0
Skip <skip@usecurling.com>
2026-04-07 21:52:07 +0000

    Refactor: Optimize styles and scripts for production

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./main.css";
+import "./components/ui/sonner.css";
 
 // @skip-protected: Do not remove. Required for React rendering.
 createRoot(document.getElementById('root')!).render(<App />)
