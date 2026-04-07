d144eb4673a5c2389552fa3f144bc0d64bc419bb
Skip <skip@usecurling.com>
2026-04-07 21:52:10 +0000

    Auto-commit after sync

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "./main.css";
-import "./components/ui/sonner.css";
+import "./index.css";
 
 // @skip-protected: Do not remove. Required for React rendering.
 createRoot(document.getElementById('root')!).render(<App />)
